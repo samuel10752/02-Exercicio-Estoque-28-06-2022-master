76b59c75806415c70fa98984997bad578607ad65
Samuel <samuelcauabarbosa114@gmail.com>
2022-07-07 11:03:33 -0300

    Atualização as 11:03

--- 02-Exercicio-Estoque-28-06-2022-master/db_estoque.sql
+++ 02-Exercicio-Estoque-28-06-2022-master/db_estoque.sql
@@ -1,14 +1,6 @@
 create database estoque;
 use estoque;
 
-create table Estoque(
-id int auto_increment,
-nome varchar(70) not null,
-fabricante varchar(35) not null,
-quantidade int,
-primary key (id)
-);
-
 create table Produtos(
 id int auto_increment,
 nome varchar(70) not null,
@@ -23,4 +15,20 @@ nome varchar(70) not null,
 primary key (id)
 );
 
-select * from Fabricante
+create table Compras(
+id int auto_increment,
+quantidade varchar(70) not null,
+primary key (id)
+);
+
+create table Vendas(
+id int auto_increment,
+quantidade varchar(70) not null,
+primary key (id)
+);
+
+select * from Produtos;
+select * from Fabricante;
+select * from Compras;
+select * from Vendas
+
